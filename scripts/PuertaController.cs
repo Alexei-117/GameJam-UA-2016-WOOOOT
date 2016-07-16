37022ffd0fea6a57dedf2e324f0c60ee3c687ac7
AdrianFL <adrianfranceslillo@gmail.com>
2016-07-16 14:30:21 +0200

    Añadido script palanca

---
+++ scripts/PuertaController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuertaController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+   public void changeStatus()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ scripts/TriggerPalanca.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerPalanca : MonoBehaviour {
+
+    public PalancaController palancaController;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            palancaController.onActivated();
+        }
+	}
+}
