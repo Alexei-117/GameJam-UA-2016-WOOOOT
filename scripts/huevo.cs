9639b7c9dba38d7757d88dda9ce63ae131747202
JesusHF <jehernandez1996@gmail.com>
2016-07-16 14:59:32 +0200

    Script del huevo

---
+++ scripts/huevo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+//using System;
+using System.Collections;
+
+public class huevo : MonoBehaviour {
+
+
+    public static bool CheckpointActive = false;
+    public static Vector2 checkpoint;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            CheckpointActive = true;
+            checkpoint = (Vector2)other.transform.localPosition;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
